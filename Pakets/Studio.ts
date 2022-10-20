e295892f40a9925ef26819ba8b97c4a9d2fafadc
IvoRum <ivailo.rumenov@outlook.com>
2022-10-20 22:25:07 +0300

    naprqgash me

--- Pakets/Studio.ts
+++ Pakets/Studio.ts
@@ -1,3 +1,15 @@
 class Studio{
     //List Artist artistList;
+    private name:string;
+    private empleys:number;
+    private employes =new Map<string,Artist>;
+
+    Studio(name:string){
+
+    }
+
+
+
+
+
 }
