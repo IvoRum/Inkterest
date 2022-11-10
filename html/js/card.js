85a57fc2bbbc5f8af7d299e5d7f82b2bdd65239e
IvoRum <ivailo.rumenov@outlook.com>
2022-11-10 22:35:15 +0200

    addet style and home page

---
+++ html/js/card.js
@@ -0,0 +1,15 @@
+class card{
+    artistName;
+    tattoName;
+    discription;
+    imageTattoo;
+    card(artistName,tattooName,discription,imageTattoo){
+        this.artistName=artistName;
+        this.tattoName=tattooName;
+        this.discription=discription;
+        this.imageTattoo=imageTattoo;
+    }
+
+    
+
+}
