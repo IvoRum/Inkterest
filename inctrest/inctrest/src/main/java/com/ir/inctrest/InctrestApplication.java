23f576e942e3773a28bb9d3a1ed8d9f6c1534b76
ivorum <ivailo.rumenov@outlook.com>
2023-06-12 16:41:05 +0300

    We are back baby. Ravaving an old project for the masters work.

---
+++ inctrest/inctrest/src/main/java/com/ir/inctrest/InctrestApplication.java
@@ -0,0 +1,13 @@
+package com.ir.inctrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InctrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InctrestApplication.class, args);
+	}
+
+}
---
+++ inctrest/inctrest/src/test/java/com/ir/inctrest/InctrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ir.inctrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InctrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
