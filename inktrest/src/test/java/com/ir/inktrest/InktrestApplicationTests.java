2b8cfe11632d0eec1582b447ef3856dc5138a363
ivorum <ivailo.rumenov@outlook.com>
2023-06-25 22:20:55 +0300

    Created a new spring project

---
+++ inktrest/src/test/java/com/ir/inktrest/InktrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ir.inktrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InktrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
