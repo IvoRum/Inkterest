ed4919748ed36dc63e0aba9b8492bc32866fc4ac
100sq <67575246+100sq@users.noreply.github.com>
2023-07-16 20:19:24 +0300

    Trying to create a multy-moduler project.

---
+++ inktrest-core/src/test/java/com/ir/inktrestcore/InktrestCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ir.inktrestcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InktrestCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ inktrest-presistence/src/test/java/com/ir/inktrestpresistence/InktrestPresistenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ir.inktrestpresistence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InktrestPresistenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
