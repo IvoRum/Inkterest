a12760a125c27287734512fe82d6c2526233cf74
IvoRum <ivailo.rumenov@outlook.com>
2024-01-23 22:35:21 +0200

    Added sql scripts

--- data-grip/ink_aggregation_insert.sql
+++ data-grip/ink_aggregation_insert.sql
@@ -1,10 +1,10 @@
-insert into public."User"(id, email, name, first_name, surname_name, last_name, date_of_birth, status, time_stamp)
+insert into public.Post(id, name, image, likes) VALUES (1,'New Tattoo','tattoo.png',0);
+
+insert into public.Artist(user_id, post) VALUES (1,1);
+
+insert into public.Location(id, street, number) VALUES (1,'VichoPapazov22',22);
+
+insert into public.Ink_User(id, email, name, first_name, surname_name, last_name, date_of_birth, status, time_stamp)
 values(1,'ivo@mail.com','IvoRume','Ivaylo','Plamenov','Rumenov','2000-07-06',1,'2023-10-28');
 
-insert into public."Post"(id, name, image, likes) VALUES (1,'New Tattoo','tattoo.png',0);
-
-insert into public."Artist"(user_id, post) VALUES (1,1);
-
-insert into public."Location"(id, street, number) VALUES (1,'VichoPapazov22',22);
-
-insert into public."Studio"(id, name, location, owner, staff) VALUES (1,'Ivos studio',1,1,1);
+insert into public.Studio(id, name, location, owner, staff) VALUES (1,'Ivos studio',1,1,1);
